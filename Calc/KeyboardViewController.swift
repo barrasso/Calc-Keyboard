d1a33ab007f069b2ec2a64ea27f87ba4a1938a65
Mark Barrasso <mark.barrasso@live.com>
2015-01-15 02:04:18 -0500

    created user interface

---
+++ Calc/KeyboardViewController.swift
@@ -0,0 +1,50 @@
+//
+//  KeyboardViewController.swift
+//  Calc
+//
+//  Created by Mark on 1/15/15.
+//  Copyright (c) 2015 MEB. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    
+        // Add custom view sizing constraints here
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Perform custom UI setup here
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+    override func textWillChange(textInput: UITextInput) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+
+    override func textDidChange(textInput: UITextInput) {
+        // The app has just changed the document's contents, the document context has been updated.
+    
+        var textColor: UIColor
+        var proxy = self.textDocumentProxy as UITextDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
+            textColor = UIColor.whiteColor()
+        } else {
+            textColor = UIColor.blackColor()
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+    }
+
+}
